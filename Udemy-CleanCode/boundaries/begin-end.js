cc127a296c894e2cc7990b95e7798e20861b63c0
sovla <sovla@naver.com>
2022-05-27 16:04:37 +0900

    - boundaries 작성 완료

---
+++ Udemy-CleanCode/boundaries/begin-end.js
@@ -0,0 +1,11 @@
+/*
+    begin-end  시작과 끝의 조건식이 다른 경우 
+    경계를 포함하지만 제외하는 경우 
+*/
+
+//  함수 이름과 파라미터 명을 보고 유추 가능하도록 작성 하자
+function reservationDate(beginDate, endDate) {
+  //  ...someCode
+}
+
+reservationDate("YYYY-MM-DD", "YYYY-MM-DD");
