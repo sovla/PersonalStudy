d9cc32df6cff798a204690fb4417f1e5f131ed15
sovla <41351496+sovla@users.noreply.github.com>
2022-07-03 23:04:45 +0900

    Add 개발환경 배포환경 설정

--- NestJS/post/src/main.ts
+++ NestJS/post/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('server.port');
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
