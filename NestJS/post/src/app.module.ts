2954cd94c7933452f6b89d944407b8f77fedf25b
sovla <41351496+sovla@users.noreply.github.com>
2022-07-03 23:28:22 +0900

    Create post-Next

--- NestJS/post/src/app.module.ts
+++ NestJS/post/src/app.module.ts
@@ -1,12 +1,9 @@
-import { User } from './domain/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Post } from './domain/post.entity';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
-import { UserAuthority } from './domain/user-authority.entity';
 import { ormConfig } from './orm.config';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/config';
