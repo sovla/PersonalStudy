4626fab6a028338422488b623284c9c39c2268d9
sovla <41351496+sovla@users.noreply.github.com>
2022-08-21 22:19:05 +0900

    add(test)  플러스,마이너스,온/오프 기능 추가 및 테스트 코드 작성

--- React/counter_tdd/src/App.js
+++ React/counter_tdd/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const [isDisabled, setIsDisabled] = useState(false);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h3 data-testid="counter">{counter}</h3>
+                <div>
+                    <button
+                        onClick={() => setCounter((prev) => prev + 1)}
+                        data-testid="plus-button"
+                        disabled={isDisabled}
+                    >
+                        +
+                    </button>
+                    <button
+                        onClick={() => setCounter((prev) => prev - 1)}
+                        data-testid="minus-button"
+                        disabled={isDisabled}
+                    >
+                        -
+                    </button>
+                </div>
+                <div>
+                    <button
+                        onClick={() => setIsDisabled((prev) => !prev)}
+                        data-testid="on/off-button"
+                        style={{ backgroundColor: "blue" }}
+                    ></button>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
