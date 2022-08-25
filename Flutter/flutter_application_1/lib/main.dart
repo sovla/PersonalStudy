d9a5ce947cf4028946c2e4b6fa7747e54c2b0edf
sovla <41351496+sovla@users.noreply.github.com>
2022-08-25 11:30:25 +0900

    create(Flutter) First App

---
+++ Flutter/flutter_application_1/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BBANTO",
+      home: Grade(),
+    );
+  }
+}
+
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[800],
+      appBar: AppBar(
+        title: Text("BBANTO"),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true, // 센터
+        elevation: 0.0, // 그림자
+      ),
+    );
+  }
+}
