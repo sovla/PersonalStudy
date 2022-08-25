11b6f19500f49d7045e0b265a2b5517480434984
sovla <41351496+sovla@users.noreply.github.com>
2022-08-25 14:44:48 +0900

    add(style) BBANTO 앱 추가

--- Flutter/flutter_application_1/lib/main.dart
+++ Flutter/flutter_application_1/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "BBANTO",
       home: Grade(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -22,13 +23,102 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
-      appBar: AppBar(
-        title: Text("BBANTO"),
-        backgroundColor: Colors.amber[700],
-        centerTitle: true, // 센터
-        elevation: 0.0, // 그림자
-      ),
-    );
+        backgroundColor: Colors.amber[600],
+        appBar: AppBar(
+          title: Text("BBANTO"),
+          backgroundColor: Colors.amber[700],
+          centerTitle: true, // 센터
+          elevation: 0.0, // 그림자
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  // 동그란 아바타
+                  backgroundImage: AssetImage("assets/fire.jfif "), // 백그라운드 이미지
+                  radius: 60.0, // 크기
+                ),
+              ),
+              Divider(
+                // 줄
+                height: 60.0,
+                color: Colors.grey[850],
+                thickness: 0.5, // 굵기
+                endIndent: 30.0, // 끝에서 어느정도 떨어져야 될지
+              ),
+              Text("Name",
+                  style: TextStyle(color: Colors.white, letterSpacing: 2.0)),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text("BBANTO",
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("BBANTO POWER LEVEL",
+                  style: TextStyle(color: Colors.white, letterSpacing: 2.0)),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text("14",
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold)),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "using lightsaber",
+                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "facehero tattoo",
+                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "fire flames",
+                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                  )
+                ],
+              ),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/character.jpg'),
+                  radius: 40.0,
+                  backgroundColor: Colors.amber[600],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
