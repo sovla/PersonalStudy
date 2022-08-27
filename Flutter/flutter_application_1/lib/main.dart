6eb02860946f407c4c5fa89e22e9706218e2a01c
sovla <41351496+sovla@users.noreply.github.com>
2022-08-27 16:13:26 +0900

    add(ListviewBuilder) 리스트 뷰 빌더 활용

--- Flutter/flutter_application_1/lib/main.dart
+++ Flutter/flutter_application_1/lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MyApp());
@@ -10,69 +9,139 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "빌더 위젯 없이 스낵바/토스트메시지",
-      home: MyPage(),
-    );
+    return MaterialApp(home: ListViewPage());
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  var imageList = [
+    "assets/1.png",
+    "assets/2.png",
+    "assets/3.png",
+    "assets/4.png",
+    "assets/5.png",
+    "assets/6.png",
+    "assets/7.png",
+    "assets/8.png",
+    "assets/9.png",
+    "assets/10.png"
+  ];
+
+  var titleList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+  var description = [
+    "제1항의 지시를 받은 당해 행정기관은 이에 응하여야 한다. 대통령의 국법상 행위는 문서로써 하며, 이 문서에는 국무총리와 관계 국무위원이 부서한다. 군사에 관한 것도 또한 같다.",
+    "국정의 중요한 사항에 관한 대통령의 자문에 응하기 위하여 국가원로로 구성되는 국가원로자문회의를 둘 수 있다. 비상계엄이 선포된 때에는 법률이 정하는 바에 의하여 영장제도, 언론·출판·집회·결사의 자유, 정부나 법원의 권한에 관하여 특별한 조치를 할 수 있다.",
+    "국가는 농지에 관하여 경자유전의 원칙이 달성될 수 있도록 노력하여야 하며, 농지의 소작제도는 금지된다. 모든 국민은 행위시의 법률에 의하여 범죄를 구성하지 아니하는 행위로 소추되지 아니하며, 동일한 범죄에 대하여 거듭 처벌받지 아니한다.",
+    "정부는 회계연도마다 예산안을 편성하여 회계연도 개시 90일전까지 국회에 제출하고, 국회는 회계연도 개시 30일전까지 이를 의결하여야 한다. 국가는 균형있는 국민경제의 성장 및 안정과 적정한 소득의 분배를 유지하고, 시장의 지배와 경제력의 남용을 방지하며, 경제주체간의 조화를 통한 경제의 민주화를 위하여 경제에 관한 규제와 조정을 할 수 있다.",
+    "국가는 재해를 예방하고 그 위험으로부터 국민을 보호하기 위하여 노력하여야 한다. 국회는 상호원조 또는 안전보장에 관한 조약, 중요한 국제조직에 관한 조약, 우호통상항해조약, 주권의 제약에 관한 조약, 강화조약, 국가나 국민에게 중대한 재정적 부담을 지우는 조약 또는 입법사항에 관한 조약의 체결·비준에 대한 동의권을 가진다.",
+    "감사원은 원장을 포함한 5인 이상 11인 이하의 감사위원으로 구성한다. 근로조건의 기준은 인간의 존엄성을 보장하도록 법률로 정한다. 대통령은 법률이 정하는 바에 의하여 사면·감형 또는 복권을 명할 수 있다.",
+    "대법원은 법률에 저촉되지 아니하는 범위안에서 소송에 관한 절차, 법원의 내부규율과 사무처리에 관한 규칙을 제정할 수 있다. 계엄을 선포한 때에는 대통령은 지체없이 국회에 통고하여야 한다.",
+    "새로운 회계연도가 개시될 때까지 예산안이 의결되지 못한 때에는 정부는 국회에서 예산안이 의결될 때까지 다음의 목적을 위한 경비는 전년도 예산에 준하여 집행할 수 있다.",
+    "누구든지 병역의무의 이행으로 인하여 불이익한 처우를 받지 아니한다. 이 헌법시행 당시에 이 헌법에 의하여 새로 설치될 기관의 권한에 속하는 직무를 행하고 있는 기관은 이 헌법에 의하여 새로운 기관이 설치될 때까지 존속하며 그 직무를 행한다.",
+    "법률은 특별한 규정이 없는 한 공포한 날로부터 20일을 경과함으로써 효력을 발생한다. 국가는 모성의 보호를 위하여 노력하여야 한다. 국회의원은 현행범인인 경우를 제외하고는 회기중 국회의 동의없이 체포 또는 구금되지 아니한다.",
+  ];
+
+  void showPopup(context, title, image, description) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: Container(
+                width: MediaQuery.of(context).size.width * 0.7, // 전체 너비의 70%
+                height: 380, // 높이
+                decoration: BoxDecoration(
+                    // 박스 스타일
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white),
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        image,
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(title,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey)),
+                    Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(description,
+                            maxLines: 3, textAlign: TextAlign.center)),
+                    ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(Icons.close),
+                        label: const Text("close"))
+                  ],
+                )),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.6;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Snack Bar"),
-        centerTitle: true,
-      ),
-      body: MySnackBar(),
-    );
-  }
-}
-
-class MySnackBar extends StatelessWidget {
-  const MySnackBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          FlatButton(
-            onPressed: () {
-              flutterToast();
-            },
-            child: Text("Toast"),
-            color: Colors.blue,
-          ),
-          RaisedButton(
-              child: Text("Show Me"),
-              onPressed: () {
-                flutterToast();
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text(
-                    "asdsa",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
+      appBar: AppBar(title: const Text("ListView")),
+      body: ListView.builder(
+          itemCount: titleList.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                showPopup(context, titleList[index], imageList[index],
+                    description[index]);
+              },
+              child: Card(
+                child: Row(children: [
+                  SizedBox(
+                    // Container -> SizedBox 로 변경  Why? SizedBox의 경우 Const 키워드로 생성하기 때문에 런타임시 재생성할 필요가 없다
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(imageList[index]),
                   ),
-                  duration: Duration(milliseconds: 1000),
-                  backgroundColor: Colors.teal,
-                ));
-              }),
-        ],
-      ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Text(
+                        titleList[index],
+                        style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: width,
+                        child: Text(description[index],
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[500])),
+                      ),
+                    ]),
+                  )
+                ]),
+              ),
+            );
+          }),
     );
   }
-}
-
-void flutterToast() {
-  Fluttertoast.showToast(
-      msg: "Flutter",
-      gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.redAccent,
-      fontSize: 20.0,
-      textColor: Colors.white,
-      toastLength: Toast.LENGTH_SHORT);
 }
