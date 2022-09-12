2fa867bd8b016a06faad1b9c3d57c56bb19e2f21
sovla <41351496+sovla@users.noreply.github.com>
2022-09-13 07:59:30 +0900

    add(firebase) firebase setting & chatScreen init

--- Flutter/flutter_chatting/lib/main.dart
+++ Flutter/flutter_chatting/lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chatting/config/palette.dart';
 import 'package:flutter_chatting/screens/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
---
+++ Flutter/flutter_chatting/lib/screens/chat_screen.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _authentication = FirebaseAuth.instance;
+
+  User? loggedUser;
+
+  void getCurrentUser() {
+    try {
+      final user = _authentication.currentUser;
+      if (user != null) {
+        loggedUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat screen"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _authentication.signOut();
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.exit_to_app_sharp,
+              ))
+        ],
+      ),
+      body: Center(child: Text("Chat Screen")),
+    );
+  }
+}
