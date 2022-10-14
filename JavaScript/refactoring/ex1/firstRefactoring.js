2b61d7fbe4ead09a371528812d8324d0501f2975
sovla <41351496+sovla@users.noreply.github.com>
2022-10-14 09:01:11 +0900

    add(refactoring) Switch -> Function

--- JavaScript/refactoring/ex1/firstRefactoring.js
+++ JavaScript/refactoring/ex1/firstRefactoring.js
@@ -53,3 +53,34 @@ function statement(invoice, plays) {
   result += `적립 포인트 : ${volumeCredits}점\n`;
   return result;
 }
+
+function statementRefactoring(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+
+    let thisAmount = amountFor(perf, play);
+
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트 : ${volumeCredits}점\n`;
+  return result;
+}
