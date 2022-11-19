eaf9eb4f13e8bf693d719a7a02eabe4fd9a68d00
sovla <41351496+sovla@users.noreply.github.com>
2022-11-19 11:25:30 +0900

    add(refactoring) 함수 쪼개기, 계산 단위 분리하기

--- JavaScript/refactoring/ex1/firstRefactoring.js
+++ JavaScript/refactoring/ex1/firstRefactoring.js
@@ -1,5 +1,4 @@
-const invoices = require('./invoices.js');
-const plays = require('./plays.js');
+const createStatementData = require('./createStatementData.js');
 
 function statement(invoice, plays) {
   let totalAmount = 0;
@@ -51,82 +50,46 @@ function statement(invoice, plays) {
   result += `적립 포인트 : ${volumeCredits}점\n`;
   return result;
 }
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function volumnCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ('comedy' === playFor(aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-function usd(aNumber) {
-  // 이름 변경 format -> usd
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
-}
-
-function totalVolumnCredits() {
-  let result = 0;
-  for (let perf of invoices.performances) {
-    result += volumnCreditsFor(perf);
-  }
-  return result;
-}
-function totalAmount() {
-  let result = 0;
-  for (let perf of invoices.performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
 function statementRefactoringSwitch(invoice) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
-  for (let perf of invoice.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트 : ${totalVolumnCredits()}점\n`;
-  return result;
+  return renderPlainText(createStatementData(invoice));
 }
 
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
+
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트 : ${data.totalVolumnCredits}점\n`;
   return result;
+
+  function usd(aNumber) {
+    // 이름 변경 format -> usd
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 }
 
-module.exports = amountFor;
-
+function htmlStatement(invoice) {
+  return renderHtml(createStatementData(invoice));
+}
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer}</h1>`;
+  return result;
+  function usd(aNumber) {
+    // 이름 변경 format -> usd
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+}
 module.exports = {
   statement: statement,
   statementRefactoringSwitch: statementRefactoringSwitch,
