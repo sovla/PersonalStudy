1b40015615a727f2a231e688aff26b4667ce21d2
sovla <41351496+sovla@users.noreply.github.com>
2022-11-19 10:02:16 +0900

    add(refactoring) Switch문 덜어내기

--- JavaScript/refactoring/ex1/statement.test.js
+++ JavaScript/refactoring/ex1/statement.test.js
@@ -1,9 +1,18 @@
 const playsJson = require('./plays.js');
 const invoicesJson = require('./invoices.js');
-const statement = require('./firstRefactoring.js');
+const {
+  statement,
+  statementRefactoringSwitch,
+} = require('./firstRefactoring.js');
 
 test('statement Test', () => {
   expect(statement(invoicesJson, playsJson)).toBe(
+    '청구 내역 (고객명: BigCo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트 : 47점\n'
+  );
+});
+
+test('statementRefactoringSwitch Test', () => {
+  expect(statementRefactoringSwitch(invoicesJson, playsJson)).toBe(
     '청구 내역 (고객명: BigCo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트 : 47점\n'
   );
 });
