389a36e61be674f66a9b3286fba4d1a99c653242
sovla <41351496+sovla@users.noreply.github.com>
2022-11-19 13:20:04 +0900

    add(refactoring) 계산 부분 다형성을 이용해 분리

--- JavaScript/refactoring/ex1/createStatementData.js
+++ JavaScript/refactoring/ex1/createStatementData.js
@@ -1,4 +1,20 @@
 const plays = require('./plays');
+const {
+  PerformanceCalculator,
+  TragedyCalculator,
+  ComedyCalculator,
+} = require('./PerformanceCalculator');
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
 
 module.exports = function createStatementData(invoice) {
   const statementData = {};
@@ -10,46 +26,18 @@ module.exports = function createStatementData(invoice) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
   function totalAmount(data) {
     let result = 0;
