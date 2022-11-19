c29d19c5c3443f89f5b391dd25e4286bb78d230a
sovla <41351496+sovla@users.noreply.github.com>
2022-11-19 10:30:32 +0900

    add(refactoring) 인라인 변수 제거 및 함수 추출하기

--- JavaScript/refactoring/ex1/firstRefactoring.js
+++ JavaScript/refactoring/ex1/firstRefactoring.js
@@ -1,4 +1,4 @@
-const amountFor = require('./switch_refactoring.js');
+const plays = require('./plays.js');
 
 function statement(invoice, plays) {
   let totalAmount = 0;
@@ -50,8 +50,19 @@ function statement(invoice, plays) {
   result += `적립 포인트 : ${volumeCredits}점\n`;
   return result;
 }
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
 
-function statementRefactoringSwitch(invoice, plays) {
+function volumnCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ('comedy' === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
+}
+
+function statementRefactoringSwitch(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -63,24 +74,45 @@ function statementRefactoringSwitch(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    volumeCredits += volumnCreditsFor(perf);
 
-    let thisAmount = amountFor(perf, play);
-
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트 : ${volumeCredits}점\n`;
   return result;
 }
+
+function amountFor(aPerformance) {
+  let result = 0;
+  switch (playFor(aPerformance).type) {
+    case 'tragedy':
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+
+    case 'comedy':
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+  }
+  return result;
+}
+
+module.exports = amountFor;
 
 module.exports = {
   statement: statement,
