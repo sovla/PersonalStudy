36848796a2f3e3a582a32ec36e6079fa66bed8fa
sovla <41351496+sovla@users.noreply.github.com>
2022-11-19 10:09:20 +0900

    add(refactoring) 변수 이름 명확하게 짓기

--- JavaScript/refactoring/ex1/switch_refactoring.js
+++ JavaScript/refactoring/ex1/switch_refactoring.js
@@ -23,30 +23,34 @@
 // 1-1 perf, play는 매개변수로 넘겨주면 된다.
 // 1-2 thisAmount의 경우 함수 안에서 값이 변경되는데 이런 변수는 조심히 다뤄야 한다. 지금의 경우 리턴을 통해 값을 전달하도록 하였다.
 
-function amountFor(perf, play) {
+// function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
+  // pref -> aPerformance 명확하게 이름 변경
   // 값이 바뀌지 않는 매개변수로 전달
-  let thisAmount = 0; // 변수를 초기화 하는 코드
+  // let thisAmount = 0; // 변수를 초기화 하는 코드
+
+  let result = 0; // thisAmount -> result 리턴해주는 값은 result로 명확하게 이름 변경
   switch (play.type) {
     case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
 
     case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
 
       break;
 
     default:
       throw new Error(`알 수 없는 장르 : ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 module.exports = amountFor;
