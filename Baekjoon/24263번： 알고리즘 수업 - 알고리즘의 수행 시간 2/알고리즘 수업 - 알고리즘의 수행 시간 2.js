4d16b215867d7a5b938e78c406e21ecdf17b2697
sovla <sovla@naver.com>
2025-02-28 10:53:36 +0900

    feat(beakjoon): add 24263 problem

--- Baekjoon/24263번： 알고리즘 수업 - 알고리즘의 수행 시간 2/알고리즘 수업 - 알고리즘의 수행 시간 2.js
+++ Baekjoon/24263번： 알고리즘 수업 - 알고리즘의 수행 시간 2/알고리즘 수업 - 알고리즘의 수행 시간 2.js
@@ -9,3 +9,7 @@
 /*   Solved: 2025/02/27 12:34:06 by sovla         ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
+const fs = require("fs");
+const input = +fs.readFileSync("/dev/stdin").toString().trim();
+console.log(input);
+console.log(1);
